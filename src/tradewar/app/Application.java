bb72dda43a3b7af33930d4b9b4278ff61bec8b0c
SirTobi <Code.DataByte@gmail.com>
2013-11-09 19:57:34 +0100

    shutdownhook added

--- src/tradewar/app/Application.java
+++ src/tradewar/app/Application.java
@@ -1,38 +1,55 @@
 package tradewar.app;
 
 
+import java.awt.EventQueue;
+
 import tradewar.api.IApp;
 import tradewar.app.gui.ApplicationWindow;
 import tradewar.app.gui.TestScene;
 import tradewar.log.ILogStream;
 import tradewar.log.Log;
 
-public class Application implements IApp {
+public class Application implements IApp, Runnable {
 
 	private Log log = new Log(ILogStream.sys, "app");
 	ApplicationWindow appWin;
 	
-	public Application() {
+	public Application(String[] args) {
 
 	}
-	
-	public void start(String[] args) {
+
+	public void run() {
+
 		log.info("Start application...");
 		
 		appWin = new ApplicationWindow(log.getStream(), "MainWindow");
 		appWin.setScene(new TestScene(appWin));
-	}
-	
-	public void run() {
+		
 		appWin.setVisible(true);
 	}
 	
+
+	private void shutdown() {
+
+		log.info("Application closed!");
+	}
+
+	private Thread getShutdownHook() {
+		
+		return new Thread() {
+			@Override
+			public void run() {
+				shutdown();
+			}
+		};
+	}
 	
 	public static void main(String[] args) {
-		Application app = new Application();
 		
-		app.start(args);
-		app.run();
+		Application app = new Application(args);
+		EventQueue.invokeLater(app);
+		
+		Runtime.getRuntime().addShutdownHook(app.getShutdownHook());
 	}
 
 }
